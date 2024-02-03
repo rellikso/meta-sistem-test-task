95156d045f76fb8792cfba13310d2f9595defa28
Maksim A. Sheptookha <rellikso@gmail.com>
2024-02-03 11:41:31 +0200

    Final commit.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UploadController::class, 'index'])->name('upload.index');
+Route::post('/', [UploadController::class, 'store'])->name('upload.store');
